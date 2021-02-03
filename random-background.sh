50b31c4d982418a89a19f3f1223b16a8e500fd87
James Gray <zaargy@gmail.com>
2021-02-03 19:52:23 +0000

    hardcore wallpaper

--- random-background.sh
+++ random-background.sh
@@ -4,5 +4,6 @@ find "$WALLPAPER_DIR" -maxdepth 1 -type f | \
   sort -R | tail -1 | \
 while read file; do
   echo "Setting wallpaper to $file..."
-  feh --bg-fill "$file" &
+  # feh --bg-fill "$file" &
+  feh --bg-fill "$WALLPAPER_DIR/abstract-fractal-desktop-b1-2560x1440.jpg" &
 done
