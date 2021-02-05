bd431f948e72fef05fb99f9b683d1226897a34df
James Gray <zaargy@gmail.com>
2021-02-05 13:01:15 +0000

    disable streamer from lock script for now

--- lock.sh
+++ lock.sh
@@ -2,5 +2,5 @@
 
 WEBCAM_FILE="$(date +%Y%m%d_%H%M%S)-shot.jpeg"
 
+#streamer -b 32 -o "$WEBCAM_DIR/$WEBCAM_FILE"
 i3lock -i "$WALLPAPER_DIR/92i3mebsma021.png" && sleep 1
-streamer -b 32 -o "$WEBCAM_DIR/$WEBCAM_FILE"
