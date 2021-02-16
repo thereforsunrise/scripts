f9e9c96991d8f050eb82126d9e5f959c13052365
James Gray <zaargy@gmail.com>
2021-02-16 20:31:07 +0000

    add dyndns update script

---
+++ dyndns.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+UPDATE_URL="$1"
+UPDATE_SECRET="$2"
+SUBDOMAIN="$3"
+IP_TYPE="${4:-external}"
+
+if [[ "$IP_TYPE" == "external" ]]; then
+  MY_IP=$(curl -s https://myexternalip.com/raw)
+else
+  MY_IP=$(hostname -I | awk '{print $1}')
+fi
+
+curl "$UPDATE_URL?secret=$UPDATE_SECRET&domain=$SUBDOMAIN&addr=$MY_IP"
