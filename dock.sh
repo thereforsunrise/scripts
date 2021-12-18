9956819281ef15043e68826c6f2190a1838c37cc
James Gray <zaargy@gmail.com>
2021-12-18 12:40:54 +0000

    new scripts

--- dock.sh
+++ dock.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-xrandr --output LVDS-1 --off
-xrandr --output HDMI-1 --primary
+mode="${1:-home}"
+
+home() {
+  xrandr \
+    --output LVDS-1 --auto \
+    --left-of HDMI-1 --primary
+}
+
+mainonly() {
+  xrandr \
+    --output LVDS-1 --off \
+    --output HDMI-1 --primary
+}
+
+laptop() {
+    xrandr \
+    --output LVDS-1 --auto --primary \
+    --output HDMI-1 --off
+}
+
+eval "$mode"
---
+++ undock.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xrandr --output HDMI-1 --off
+xrandr --output LVDS-1 --on --primary
