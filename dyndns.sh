56e2fb74ee71b365d5753047407a9f482a0043b6
James Gray <zaargy@gmail.com>
2021-12-22 10:49:19 +0000

    stuff

--- dyndns.sh
+++ dyndns.sh
@@ -11,4 +11,4 @@ else
   MY_IP=$(hostname -I | awk '{print $1}')
 fi
 
-curl -s -L "$UPDATE_URL?secret=$UPDATE_SECRET&domain=$SUBDOMAIN&addr=$MY_IP"
+curl -k -L "$UPDATE_URL?secret=$UPDATE_SECRET&domain=$SUBDOMAIN&addr=$MY_IP"
