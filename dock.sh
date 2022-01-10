8b967c3fc5f9508e41989eea3a485cef7c77527d
James Gray <zaargy@gmail.com>
2022-01-10 06:44:14 +0000

    fix xrandr commands

--- dock.sh
+++ dock.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
+# just a script so i remember a bunch of xrandr commands
 
-mode="${1:-home}"
+mode="${1:-studio}"
 
-home() {
+studio() {
   xrandr \
     --output LVDS-1 --auto \
     --left-of HDMI-1 --primary
 }
 
-mainonly() {
+studiomainonly() {
   xrandr \
     --output LVDS-1 --off \
     --output HDMI-1 --primary
 }
 
-laptop() {
+office() {
+  xrandr \
+     --output eDP-1 --off \
+     --output HDMI-1 --primary
+}
+
+x230only() {
     xrandr \
     --output LVDS-1 --auto --primary \
+    --output HDMI-1 --off
+}
+
+x1only() {
+  xrandr \
+    --output eDP-1-1 --auto --primary \
     --output HDMI-1 --off
 }
 
