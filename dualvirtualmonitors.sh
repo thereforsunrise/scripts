ff5074b7b3a06ff6c59a9da006736d0d6f3b476a
James Gray <zaargy@gmail.com>
2022-02-02 18:19:51 +0000

    fix virtual monitors script

--- dualvirtualmonitors.sh
+++ dualvirtualmonitors.sh
@@ -3,26 +3,32 @@
 # TODO: change this script to accept params of monitor and calculate values
 # HDMI-1 connected primary 2560x1440+0+0 (normal left inverted right x axis y axis) 708mm x 399mm
 
-COMMAND=$1
-DISPLAY=${2:-HDMI-1}
+set -e
 
-WIDTH_PX=${3:-1280}
-WIDTH_MM=${4:-354}
-WIDTH_MM_2=${5:-355}
+COMMAND=$1
+DISPLAY=${2:-DP-2}
+
+HEIGHT_PX="1440"
+WIDTH_PX="1280"
+WIDTH_MM="354"
+WIDTH_MM_2="355"
 
 if [[ "$COMMAND" == "add" ]]; then
+  cat <<EOF
   xrandr \
-    --setmonitor "$DISPLAY-1" \
-    "$WIDTH_PX/${WIDTH_MM}x${WIDTH_PX}/1+0+0" \
+    --setmonitor "${DISPLAY}-1" \
+    "$WIDTH_PX/${WIDTH_MM}x${HEIGHT_PX}/399+0+0" \
     "$DISPLAY"
-    
   xrandr \
-    --setmonitor "$DISPLAY-2" \
-    "$WIDTH_PX/${WIDTH_MM_2}x${WIDTH_PX}/1+${${WIDTH_PX}}+0" \
+    --setmonitor "${DISPLAY}-2" \
+    "$WIDTH_PX/${WIDTH_MM_2}x${HEIGHT_PX}/399+$WIDTH_PX+0" \
     none
+EOF
 else
-  xrandr --delmonitor "$DISPLAY-1"
-  xrandr --delmonitor "$DISPLAY-2"
+  cat <<EOF
+  xrandr --delmonitor "${DISPLAY}-1"
+  xrandr --delmonitor "${DISPLAY}-2"
+EOF
 fi
 
-i3-msg restart
+echo i3-msg restart
